6c85fd41ff8b2964955c27960730308604050c2f
42LoCo42 <11213kbm@gmail.com>
2019-08-17 19:48:26 +0200

    Added installation script for required libraries.

---
+++ install.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/42LoCo42/libcursedcat.git
+cd libcursedcat
+make
+sudo make install
+cd ..
+
+git clone https://github.com/42LoCo42/libmss.git
+cd libmss
+make
+sudo make install
+cd ..
