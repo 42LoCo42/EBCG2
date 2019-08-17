aa0192b1f3073f444b07c803bde046f7c383f940
42LoCo42 <11213kbm@gmail.com>
2019-08-17 20:17:54 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
+echo "Installing CursedCat..."
 git clone https://github.com/42LoCo42/libcursedcat.git
 cd libcursedcat
 make
 sudo make install
 cd ..
 
+echo "Installing MSS..."
 git clone https://github.com/42LoCo42/libmss.git
 cd libmss
 make
 sudo make install
 cd ..
+
+echo "Creating EBCG2..."
+make
+make clean
+rm -rf libcursedcat
+rm -rf libmss
